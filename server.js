969294df7bb8e7391fec0444985edbfcbf9d80a9
Emmanuel Ngufor <esngufor@myseneca.ca>
2024-05-22 00:35:26 -0400

    modified code to use the path method

--- server.js
+++ server.js
@@ -21,10 +21,14 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 // set view engine for EJS
 app.set("view engine", "ejs");
-app.set('views', __dirname + '/views');      // ---> Changed due to vercel hosting
+// app.set('views', __dirname + '/views');      // ---> Changed due to vercel hosting
+app.set('views', path.join(__dirname, 'views'));  // ---> Original  ---> Changed due to vercel hosting
+
+
 
 // app.use(express.static("public")); // Middleware to handle  static files like CSS and Images from public directory
-app.use(express.static(__dirname + '/public'));    // ---> Changed due to vercel hosting
+// app.use(express.static(__dirname + '/public'));    // ---> Changed due to vercel hosting
+app.use(express.static(path.join(__dirname, 'public')));  // ---> Original --> Changed due to vercel hosting
 app.use(express.urlencoded({ extended: true })); // Middleware to parse form data : application/x-www-form-urlencoded
 
 // configure session middleware : setup client-sessions to handle sessions
